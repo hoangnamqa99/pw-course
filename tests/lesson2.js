2ba6f2c076058cedbc54379e9aea1ec8dfdae11b
hoangnam <hoangnamqa99@gmail.com>
2025-12-19 18:45:04 +0700

    feat : thêm bài tập if vòng lặp '

---
+++ tests/lesson2.js
@@ -0,0 +1,6 @@
+console.log(3 % 3);
+console.log(13 % 3);
+console.log(" ket qua phep tinh chia lay du la :", 25 / 4);
+const slogan1 = " Học đi đôi với hành ";
+const slogan2 = " Học thật vui ";
+console.log(slogan1 + slogan2);
---
+++ tests/vonglap.js
@@ -0,0 +1,2 @@
+for (let i = 1; i < 100 ; i++) { if (i % 2 == 0) { console.log(" Số i là số chẵn :", i); } else { console.log(" Số i là số lẻ :", i); } }
+
